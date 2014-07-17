0f78f369513c9673703779387762f7132e9617c9
Juan Biondi <juan.biondi@gmail.com>
2014-07-17 17:18:35 -0300

    Instalado el driver de la DB
    Se puso en el programa java el conector con la base de datos, habría que
    probarlo y hacer un test de la conexión.
    Si podés, estaría bueno que me completes algunos de los query a la db
    así probamos el sistema funcionando.
    En la clase db connection está documentado como se hacen los query y los
    update a la db.
    Dejalo en la sintaxis de los query, yo hago elresto de rellenar los
    campos y demás ;)

--- Wrokspace_MedicalRecordSystem/Client/src/database/DatabaseConnector.java
+++ Wrokspace_MedicalRecordSystem/Client/src/database/DatabaseConnector.java
@@ -1,5 +1,8 @@
 package database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -14,12 +17,69 @@ import exceptions.UserPasswordException;
 
 public class DatabaseConnector implements PatientManager, StudyManager{
 	
-	private String server_ip;
-	private int server_port;
+	private String server_ip = "localhost";
+	private int server_port = -1;
+	private String database_name = "mrs_db";
+	
+	private boolean debug = true;
+	private void log(String message){
+		if(debug)
+			System.out.println(message);
+	}
+	
+	private Connection connection = null;
+	
+	public DatabaseConnector() throws UserPasswordException, ConnectionException{
+		this("root", "");
+	}
 
 	public DatabaseConnector(String username, String password) throws UserPasswordException, ConnectionException{
-		
+		try
+		{
+			log("Registro de driver");
+			Class.forName("com.mysql.jdbc.Driver");
+
+			String url = "jdbc:mysql:"+server_ip;
+			if(server_port != -1){
+				url += ":"+server_port;
+			}
+			url += "/"+database_name;
+			
+			log("Intento de conexión a la db: "+url);
+			connection = DriverManager.getConnection (url,username, password);
+			log("Conectado con la DB");
+		} catch (SQLException e) {
+			e.printStackTrace();
+			throw new ConnectionException(e.getMessage());
+		} catch (Exception e){
+			e.printStackTrace();
+			throw new ConnectionException(e.getMessage());
+		}
 	}
+	
+	/*
+	 * (non-Javadoc)
+	 * @see entitiesManagers.StudyManager#getPatientStudies(entities.Patient)
+	 * 
+	 * Para crear una statement a la db:
+	 * Statement st = connection.createStatement();
+	 * 
+	 * Para ejecutar un update (cualquier cosa que modifique la db):
+	 * st.executeUpdate(<<SQL DE UN UPDATE O ALGO QUE NO DEVUELVA NADA>>);
+	 * 
+	 * Para ejecutar un query (cosas que traigan resultados):
+	 * ResultSet rs = st.executeQuery(<<SQL QUERY>>); 
+	 * while (rs.next()){
+	 * 	//ALGO QUE HAGAS A CADA LÍNEA DEL RESULTADO
+	 * 	//POR EJEMPLO
+	 * 	System.out.println("nombre="+rs.getObject("nombre")); //Hay getInt() y varios más
+	 * 	//DONDE nombre ES EL NOMBRE DEL CAMPO
+	 * }
+	 * rs.close();
+	 * 
+	 * Siempre al final hay que cerrar el statement:
+	 * st.close();
+	 */
 
 	public List<Study> getPatientStudies(Patient patient)
 			throws NoConnectedException {
